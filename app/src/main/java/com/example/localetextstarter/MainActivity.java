38c7cfd8f284f8853f9416c3b54ad6ccf70b1a9a
Salwa Aurellia <salwaaurellianuza@gmail.com>
2023-04-01 11:00:56 +0700

    Initial Commit

--- app/src/main/java/com/example/localetextstarter/MainActivity.java
+++ app/src/main/java/com/example/localetextstarter/MainActivity.java
@@ -9,15 +9,39 @@ import android.provider.Settings;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.text.DateFormat;
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.Currency;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class MainActivity extends AppCompatActivity {
+
+    Button totalButton;
+    TextView total_price;
+    EditText input_amount;
+    TextView currency;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        totalButton = findViewById(R.id.total_btn);
+        total_price = findViewById(R.id.price);
+        input_amount = findViewById(R.id.amount);
+        currency = findViewById(R.id.currency);
+        Locale currentLocale = getResources().getConfiguration().locale;
+        Currency currentCurrency = Currency.getInstance(currentLocale);
+        currency.setText("("+currentCurrency.getDisplayName()+")");
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -26,6 +50,24 @@ public class MainActivity extends AppCompatActivity {
                 showHelp();
             }
         });
+        totalButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                double total = Double.parseDouble(input_amount.getText().toString())*100;
+
+                NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(currentLocale);
+                String results = currencyFormatter.format(total);
+                total_price.setText(results);
+            }
+        });
+
+        Date myDate = new Date();
+        long expirationDate = myDate.getTime() +
+                TimeUnit.DAYS.toMillis(3);
+        myDate.setTime(expirationDate);
+        String formatDate = DateFormat.getDateInstance().format(myDate);
+        TextView expiredTextView = findViewById(R.id.date);
+        expiredTextView.setText(formatDate);
     }
 
     /**
@@ -64,6 +106,11 @@ public class MainActivity extends AppCompatActivity {
         if (id == R.id.action_help) {
             Intent intent = new Intent(this, HelpActivity.class);
             startActivity(intent);
+            return true;
+        }
+        else if (id == R.id.action_language) {
+            Intent languageIntent = new Intent(Settings.ACTION_LOCALE_SETTINGS);
+            startActivity(languageIntent);
             return true;
         }
         return super.onOptionsItemSelected(item);
